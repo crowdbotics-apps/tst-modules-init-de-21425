d91c9b9d1544cabe33cbcc44d712cdd9c6f59d61
Daniel <daniel.s@crowdbotics.com>
2021-04-07 23:44:20 +0100

    add nullcheck

--- modules/index.js
+++ modules/index.js
@@ -1,7 +1,8 @@
 import { getPropertyMap, getModules } from "./utils.js";
 import * as mods from "glob:./**/index.js";
 
-export const modules = getModules(mods);
+const packages = mods || [];
+export const modules = getModules(packages);
 export const initialRoute = modules[0].title;
 export const slices = Object.entries(getPropertyMap(modules, "slice"));
 export const navigators = Object.entries(getPropertyMap(modules, "navigator"));
