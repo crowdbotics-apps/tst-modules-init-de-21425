f509b10877dd63ebbee297aa5365922f1ee4c9e0
Daniel <daniel.s@crowdbotics.com>
2021-04-07 22:55:56 +0100

    add scaffold metro config

--- metro.config.js
+++ metro.config.js
@@ -5,13 +5,29 @@
  * @format
  */
 
+const path = require('path');
+const extraNodeModules = {
+  '@modules': path.resolve(__dirname, 'modules'),
+};
+const watchFolders = [
+  path.resolve(__dirname, 'modules')
+];
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: true,
+        inlineRequires: false,
       },
     }),
   },
+  resolver: {
+    extraNodeModules: new Proxy(extraNodeModules, {
+      get: (target, name) =>
+        //redirects dependencies referenced from modules to local node_modules
+        name in target ? target[name] : path.join(process.cwd(), "node_modules", name),
+    }),
+  },
+  watchFolders,
+  resetCache: true
 };
